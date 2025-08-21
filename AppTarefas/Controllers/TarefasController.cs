56e8f9ae2a778f6baefdee2e0982c5ed9d50a4bf
Prof. Cristiano <cristiano.paula@docente.senai.br>
2025-08-21 11:51:54 -0300

    Adiciona funcionalidades de CRUD para tarefas
    Implementa métodos para editar, visualizar detalhes e excluir tarefas no controlador `TarefasController.cs`. Melhora o estilo dos formulários em `Create.cshtml` e adiciona navegação em `Index.cshtml`. Cria as páginas `Delete.cshtml`, `Details.cshtml` e `Edit.cshtml` para gerenciar tarefas de forma mais intuitiva.

--- AppTarefas/Controllers/TarefasController.cs
+++ AppTarefas/Controllers/TarefasController.cs
@@ -39,6 +39,62 @@ namespace AppTarefas.Controllers
             return View(tarefa);
         }
 
+        // GET: Tarefas/Edit/1
+        public IActionResult Edit(int id)
+        {
+            // var tarefa = _tarefas[id];  // Trabalhando com Lista
+            var tarefa = _tarefas.FirstOrDefault(t => t.TarefaId == id);
+            return View(tarefa);
+        }
+
+        // POST: Tarefas/Edit/1
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Tarefa tarefaAtualizada)
+        {
+            var tarefa = _tarefas.FirstOrDefault(t => t.TarefaId == id);
+
+            tarefa.Titulo = tarefaAtualizada.Titulo;
+            tarefa.Descricao = tarefaAtualizada.Descricao;
+            tarefa.Concluida = tarefaAtualizada.Concluida;
+
+            return RedirectToAction("Index");
+
+        }
+
+        // GET: Tarefas/Details/1
+        public IActionResult Details(int id)
+        {
+            // var tarefa = _tarefas[id];  // Trabalhando com Lista
+            var tarefa = _tarefas.FirstOrDefault(t => t.TarefaId == id);
+            return View(tarefa);
+        }
+
+
+        // GET: Tarefas/Delete/1
+        public IActionResult Delete(int id)
+        {
+            // var tarefa = _tarefas[id];  // Trabalhando com Lista
+            var tarefa = _tarefas.FirstOrDefault(t => t.TarefaId == id);
+            return View(tarefa);
+        }
+
+        // POST: Tarefas/Delete/1
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var tarefa = _tarefas.FirstOrDefault(t => t.TarefaId == id);
+            if (tarefa != null)
+            {
+                _tarefas.Remove(tarefa);
+            }
+            return RedirectToAction("Index");
+        }
+
+
+
+
 
     }
 }
